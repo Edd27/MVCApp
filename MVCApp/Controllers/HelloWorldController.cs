f8ce616ed92d017622e3a24abab3ed2ca815cbc9
edgar <edgar@DESKTOP-H6FR6NC>
2024-03-08 22:07:38 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = $"Hello {name}";
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        //GET: /HelloWorld
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //GET: /HelloWorld/Welcome
+        //Requires using System.Text.Encodings.Web
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    //return "This is the welcome action method...";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is {numTimes}");
+        //}
+    }
+}
---
+++ MVCApp/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+        [Range(1, 100)]
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string? Rating { get; set; }
+    }
+}
